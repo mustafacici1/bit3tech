f222598e7bdb78f4600b14e444318eb6340e9242
Mustafa Cici <119680195+mustafacici1@users.noreply.github.com>
2025-09-18 03:19:02 +0300

    Tüm dosyalar yeniden eklendi

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,5 @@ export const TABLES = {
 };
 
 export const socialLinks = {
-  instagram: "https://www.instagram.com/joyco.studio",
-  x: "https://x.com/joyco_studio",
-  github: "https://github.com/joyco-studio",
+  instagram: "https://www.instagram.com/bit3.tech",
 };
